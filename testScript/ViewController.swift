30539692f635e87e0658862c4aa77863fb85aed2
armin-spahic-sn <armin.spahic@sleepnumber.com>
2021-03-11 15:05:28 +0100

    test

--- testScript/ViewController.swift
+++ testScript/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // ara
+        // amigo cavjron
         // Do any additional setup after loading the view.
     }
 
